66319d05ca0d57ce6ec5fdac745ae9a184c80e38
Hao Nguyen <haonh9x@gmail.com>
2024-10-14 15:23:26 +0700

    upadte

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
+=======
+pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+>>>>>>> 49a43f0 (upadte files)
